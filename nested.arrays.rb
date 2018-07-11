66e33ea3a7ad42f5f4790e1c6d6439673d8c669b
Amanda Richardson <amanda162000@gmail.com>
2018-07-11 19:03:03 +0000

    automatically backed up by learn

--- nested.arrays.rb
+++ nested.arrays.rb
@@ -1,3 +1,10 @@
+# We have already made learned some great way to store information such as arrays or hashes but what if we want to store more information about a variable or key?
+# We use Nested Data Structures!
+#   what are nested arrays?
+#     They are data organizations that are separately identifiable, they are a larger data organization nested within the larger organization
+#       They larger organization in this example if KlossyPossee
+# Arrays can hold more than just strings, they can hold other arrays! This would be an example of a Nested Data Structure
+
 KlossyPossee = [
   ["Caroline", "Vivian", "Jessica", "Madhu"],
   ["Azya ", "Natalie", "ChaCha", "Claudia"],
@@ -5,11 +12,6 @@ KlossyPossee = [
   ["Sara", "Naquaiviera", "Zoe", "Karlie Kloss"],
   ["Vlad", "Tierraney", "Janice", "Amanda"]
 ]
-# We have already made learned some great way to store information such as arrays or hashes but what if we want to store more information about a variable or key?
-# We use Nested Data Structures!
-#   what are nested arrays?
-#     They are data organizations that are separately identifiable, they are a larger data organization nested within the larger organization
-#       They larger organization in this example if KlossyPossee
 
 
 # Lets try to find someones name within the nested array!
